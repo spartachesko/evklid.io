c75159a7922f02d028a82b3ce7214c81ccca7716
borisov_spartak <fichker@gmail.com>
2023-01-29 01:00:24 +0300

    Модуль 14/ исправлены 5/6 замечаний

--- 13_javascript/js/main.js
+++ 13_javascript/js/main.js
@@ -4,3 +4,20 @@ document.querySelector(".burger").addEventListener("click", function() {
 document.querySelector(".nav__close").addEventListener("click", function() {
   document.querySelector(".header__nav").classList.remove("active");
 })
+
+document.querySelector(".faq__item__top").addEventListener("click", function() {
+  document.querySelector(".faq__item__top__btn").classList.add("active");
+})
+
+document.querySelector(".faq__item__top").addEventListener("click", function() {
+  document.querySelector(".faq__item__top__btn").classList.remove("active");
+})
+
+document.querySelector(".faq__item__top__btn").addEventListener("click", function() {
+  document.querySelector(".faq__item__top__btn").classList.add("active");
+})
+
+document.querySelector(".faq__item__top").addEventListener("click", function() {
+  document.querySelector(".faq__item__top__btn").classList.remove("active");
+})
+
